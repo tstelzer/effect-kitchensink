f787b4a4431632f86be67b06b9723a1e57e4cf7b
Timm Stelzer <timmstelzer@gmail.com>
2023-01-27 16:11:46 +0100

    chore: boilerplate

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "@tstelzer/effect-prelude";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [ nodejs-18_x ];
+        shellHook = ''
+          mkdir -p .corepack && corepack enable --install-directory="./.corepack"
+          export PATH=$PATH:$(cd .corepack; pwd)
+        '';
+      };
+    };
+}
