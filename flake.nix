06a2df9139a0e92f29578b340bd4934df08f6b00
Timm Stelzer <timmstelzer@gmail.com>
2023-01-28 14:40:04 +0100

    feat: rename repository/package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "@tstelzer/effect-prelude";
+  description = "@tstelzer/effect-kitchensink";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
