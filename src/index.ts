2846a5719c813e3465d3e898ef363fcb3db668b8
Timm Stelzer <timmstelzer@gmail.com>
2023-02-17 21:38:51 +0100

    feat: remove Config
    This is covered by @effect/io/Config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * as fs from './fs.js';
 export type {Parser} from './Parser.js';
-export * as Config from './Config.js';
 export * as MongoDB from './MongoDB.js';
 export * as zod from './zod.js';
